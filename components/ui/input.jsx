813019dc601e8db5f9feb7145e9e8eca0c30a0aa
denandr30 <doar.denisa@gmail.com>
2026-03-16 15:08:10 +0200

    Update input.jsx

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -1,3 +1,3 @@
-export function Input(props) {
-  return <input {...props} className="border p-2 rounded w-full" />;
+export function Input({ className = "", ...props }) {
+  return <input {...props} className={className} />;
 }
