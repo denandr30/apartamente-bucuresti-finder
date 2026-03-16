55deb4ece701adfb62def9d01727320a34bbc20f
denandr30 <doar.denisa@gmail.com>
2026-03-16 15:01:03 +0200

    Create badge.jsx

---
+++ components/ui/badge.jsx
@@ -0,0 +1,10 @@
+export function Progress({ value }) {
+  return (
+    <div className="w-full bg-gray-200 rounded h-2">
+      <div
+        className="bg-green-500 h-2 rounded"
+        style={{ width: value + "%" }}
+      />
+    </div>
+  );
+}
